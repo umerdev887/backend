374cf368a87378c7f4084dab481a4d959596b17c
umerdev887 <umer.dev887@gmail.com>
2024-03-12 17:32:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
